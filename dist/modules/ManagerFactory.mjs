ef3b1fc53b10b412cb162fa190fa43b5d0acbfbe
Forien <mr.forien@gmail.com>
2023-07-26 06:22:47 +0200

    alphabetical order because why not

--- dist/modules/ManagerFactory.mjs
+++ dist/modules/ManagerFactory.mjs
@@ -1,24 +1,24 @@
-import DND5eManager from "./managers/DND5eManager.js";
 import Error from "./utility/Error.js";
 import {constants} from "./constants.mjs";
-import WFRP4eManager from "./managers/WFRP4eManager.js";
-import PF2eManager from "./managers/PF2eManager.js";
+import DND5eManager from "./managers/DND5eManager.js";
 import PF1Manager from "./managers/PF1Manager.js";
+import PF2eManager from "./managers/PF2eManager.js";
 import SFRPGManager from "./managers/SFRPGManager.js";
+import WFRP4eManager from "./managers/WFRP4eManager.js";
 
 export default class ManagerFactory {
   static getManagerBySystem(system) {
     switch (system) {
       case 'dnd5e':
         return DND5eManager;
-      case 'wfrp4e':
-        return WFRP4eManager;
-      case 'pf2e':
-        return PF2eManager;
       case 'pf1':
         return PF1Manager;
+      case 'pf2e':
+        return PF2eManager;
       case 'sfrpg':
         return SFRPGManager;
+      case 'wfrp4e':
+        return WFRP4eManager;
       default:
         throw new Error(`System ${system} is not supported by ${constants.moduleLabel}.`)
     }
