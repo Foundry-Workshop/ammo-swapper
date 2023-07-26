6ea1217f4abb8e570b16c7ddbee6af0b2d31929b
Forien <mr.forien@gmail.com>
2023-07-26 03:48:00 +0200

    added PF2e support

--- dist/modules/ManagerFactory.mjs
+++ dist/modules/ManagerFactory.mjs
@@ -2,6 +2,7 @@ import DND5eManager from "./managers/DND5eManager.js";
 import Error from "./utility/Error.js";
 import {constants} from "./constants.mjs";
 import WFRP4eManager from "./managers/WFRP4eManager.js";
+import PF2eManager from "./managers/PF2eManager.js";
 
 export default class ManagerFactory {
   static getManagerBySystem(system) {
@@ -10,6 +11,8 @@ export default class ManagerFactory {
         return DND5eManager;
       case 'wfrp4e':
         return WFRP4eManager;
+      case 'pf2e':
+        return PF2eManager;
       default:
         throw new Error(`System ${system} is not supported by ${constants.moduleLabel}.`)
     }
---
+++ dist/modules/managers/PF2eManager.js
@@ -0,0 +1,91 @@
+import BaseManager from "./BaseManager.js";
+import {constants, settings} from "../constants.mjs";
+
+export default class PF2eManager extends BaseManager {
+  /**
+   *
+   * @return {_WeaponPF2e2[]}
+   */
+  static get weapons() {
+    const actor = game.user.character;
+    const items = actor.items;
+    const checkEquipped = game.settings.get(constants.moduleId, settings.onlyEquipped);
+    const weapons = items.filter(i => (i.type === 'weapon' && (checkEquipped ? i.system.equipped.carryType === 'held' : true) && i.requiresAmmo === true));
+    const ammunition = this.ammunition;
+
+    return weapons.map(w => {
+      let selected = ammunition.find(a => a._id === w.system.selectedAmmoId);
+
+      return {
+        _id: w._id,
+        img: w.img,
+        name: w.name,
+        selected: selected,
+        ammunition: ammunition,
+        equipped: !checkEquipped ? w.system.equipped.carryType === 'held' : false
+      }
+    });
+  }
+
+  /**
+   *
+   * @return {_WeaponPF2e2[]}
+   */
+  static get ammunition() {
+    const actor = game.user.character;
+    const items = actor.items;
+    // No ammo types in PF2e?
+    let ammo = items.filter(i => i.type === 'consumable' && i.system.consumableType.value === 'ammo');
+
+    return ammo.map(a => ({
+      _id: a._id,
+      img: a.img,
+      name: a.name,
+      quantity: a.system.quantity
+    }));
+  }
+
+  /**
+   *
+   * @param {string} weaponId
+   * @param {string} ammoId
+   */
+  static async setAmmunition(weaponId, ammoId) {
+    const actor = game.user.character;
+    const weapon = actor.items.find(i => i.id === weaponId);
+
+    if (weapon) {
+      await weapon.update({'system.selectedAmmoId': ammoId});
+    }
+  }
+
+  /**
+   *
+   * @param {string} weaponId
+   */
+  static async equipWeapon(weaponId) {
+    const actor = game.user.character;
+    const weapon = actor.items.get(weaponId).toObject();
+
+    if (weapon.system.equipped.carryType === "held") {
+      weapon.system.equipped.handsHeld = 0;
+      weapon.system.equipped.carryType = 'worn';
+    } else {
+      let usage = weapon.system.usage.value;
+      let hands;
+      switch (usage) {
+        case "held-in-two-hands":
+          hands = 2;
+          break;
+        case "held-in-one-hand":
+        default:
+          hands = 1;
+      }
+
+      weapon.system.equipped.carryType = 'held';
+      weapon.system.equipped.handsHeld = hands;
+    }
+
+    return actor.updateEmbeddedDocuments("Item", [weapon]);
+  }
+}
